1e2d62ee00ef5187647c3b0a0fbabe84926ec2c6
bryanchristopher <bryanchristttt@gmail.com>
2023-01-17 18:53:22 +0700

    yes

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angularTheme';
+  email = "bryan.christopher49@gmail.com";
+  nama = "bryan christopher";
+  nim = "202102276";
+}
